8bfa8fcd3246db0d71585721e53436832ea5e890
dextergb <dextergb@rogers.com>
2021-06-04 01:12:14 +0000

    sum nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // let sum = 0;
+  // for (let i = 0; i < array.length; i++) {
+  //   if (typeof array[i] == "number") {
+  //     sum += array[i];
+  //   } else if (array[i] instanceof Array) {
+  //     sum += sumItems(array[i]);
+  //   }
+  // }
+  // return sum;
+  let flat = array.flat(Infinity);
+  let summed = flat.reduce((a, b) => {
+    return a + b;
+  });
+  return summed;
 }
 
 module.exports = sumItems;
